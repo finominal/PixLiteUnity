4bc80654d67fc5adc04cdc008d78b205c1c27b5e
fin.mccarthy <fin.mccarthy@carsales.com.au>
2016-08-10 18:47:51 +1000

    Added Unit Script for reference. This script worked.

---
+++ PixliteUnityArtnet/UnityScript/TestArtnet2.cs
@@ -0,0 +1,51 @@
+﻿using UnityEngine;
+using PixliteForUnity;
+using System.Collections.Generic;
+
+public class TestArtnet2 : MonoBehaviour {
+
+    // Use this for initialization
+    Pixlite pixlite;
+    int counter = 0;
+    int numPixelsPerString = 144;
+
+    void Start () {
+	
+	}
+
+    void Awake()
+    {
+        pixlite = new Pixlite();
+    }
+	
+	// Update is called once per frameE
+	void Update () {
+
+        var section1 = new List<Color32>();
+        var section2 = new List<Color32>();
+
+        var allSections = new List<List<Color32>>();
+
+        for (int i = 0; i < numPixelsPerString; i++)
+        {
+            if (i == counter % numPixelsPerString)
+            {
+                section2.Add(new Color32 { g = 10 });
+            }
+            else
+            {
+                section2.Add(new Color32());
+            }
+        }
+
+        allSections.Add(section1);
+        allSections.Add(section2);
+
+        pixlite.SendAllSections(allSections);
+
+        Debug.Log(counter.ToString());
+
+        counter++;
+
+    }
+}

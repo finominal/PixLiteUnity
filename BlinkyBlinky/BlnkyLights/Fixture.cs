6b7876692c49bca19a3197b49166606193d5a109
Fin McCarthy <fmccarthy@motumsimulation.com>
2019-08-16 09:25:02 +1000

    Created LedChains and Fixtures

---
+++ BlinkyBlinky/BlnkyLights/Fixture.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+namespace BlinkyLights
+{
+    public class Fixture
+    {
+        private List<LedChain> _ledChains { get; set; }
+        public Vector3 Location;
+
+        public string GetInfo = "If setting more than one chain, set all the chains xyz pixels relative to each other.";
+
+        public Fixture(Vector3 location = new Vector3())
+        {
+            _ledChains = new List<LedChain>();
+            Location = location;
+        }
+
+        public void Rotate(Vector3 rotateBy)
+        {
+            //Rotate the Location vector by the poassed in vector
+
+        }
+    }
+}
---
+++ BlinkyBlinky/BlnkyLights/PixelArray.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+namespace BlinkyLights
+{
+    public class LedChain
+    {
+        public List<Pixel> Pixels;
+
+        public string GetInfo => "This class represents a string of " +
+            "LEDs that are connected to one driver output of a controller.";
+
+        public LedChain()
+        {
+            Pixels = new List<Pixel>();
+        }
+    }
+}

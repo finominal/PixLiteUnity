86523256791c98210b139d4de92843961c66e917
Fin McCarthy <fmccarthy@motumsimulation.com>
2019-08-16 13:15:46 +1000

    Add rotation Placeholder in Fixture

--- BlinkyBlinky/BlnkyLights/Fixture.cs
+++ BlinkyBlinky/BlnkyLights/Fixture.cs
@@ -7,19 +7,38 @@ namespace BlinkyLights
     {
         private List<LedChain> _ledChains { get; set; }
         public Vector3 Location;
+        public Vector3 Rotation;
+
+        private bool _rotatedLeds = false;
 
         public string GetInfo = "If setting more than one chain, set all the chains xyz pixels relative to each other.";
 
-        public Fixture(Vector3 location = new Vector3())
+        public Fixture(Vector3 location)
         {
             _ledChains = new List<LedChain>();
             Location = location;
         }
 
-        public void Rotate(Vector3 rotateBy)
+        public void RotateFixture(Vector3 rotateBy)
         {
-            //Rotate the Location vector by the poassed in vector
+            //rotate the leds around the origin location of the fixture. 
+            //The first LED is located at the fixtures location. The rest are rotated around it?
+            if (Location != null)
+            {
+                Rotation = rotateBy;
+                RotateLeds();
+            }
+        }
 
+        private void RotateLeds()
+        {
+            foreach (var ledChain in _ledChains)
+            {
+                foreach (var pixel in ledChain.Pixels)
+                {
+                    //rotate around the origin Location by an amount specified in Rotation.
+                }
+            }
         }
     }
 }
